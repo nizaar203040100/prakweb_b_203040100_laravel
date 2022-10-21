2aec091ceb4fa7b02ea4d336b64e3aa0c71c405c
nizaar-el <elhufazhnizaar@gmail.com>
2022-10-21 23:33:09 +0700

    menyelesaikan video ketiga

---
+++ prakweb_b_203040100/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>WPU | About</title>
+</head>
+<body>
+    <1>Halaman About</1>
+    <h3><?= $nama; ?></h3>
+    <p><?= $email; ?></p>
+    <img src="img/<?= $image; ?>" alt="<?= $nama; ?>" width="200">
+</body>
+</html>
--- prakweb_b_203040100/routes/web.php
+++ prakweb_b_203040100/routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "nama" => "Nizaar El Hufazh",
+        "email" => "nizaar.20304100@mail.unpas.ac.id",
+        "image" => "Nizaar.jpg"
+    ]
+);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
